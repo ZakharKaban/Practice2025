65c5187f683670dcefd6b9a732b40885b46bf76f
ZakharKaban <zakharkaban@gmail.com>
2025-12-17 18:46:35 +0300

    FirsYes

---
+++ Project/Practice2025One/Practice2025One/AppData/AppConnect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice2025One.AppData
+{
+    internal class AppConnect
+    {
+       // public static Entities2 Model1;
+    }
+}
---
+++ Project/Practice2025One/Practice2025One/AppData/AppFrame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice2025One.AppData
+{
+    internal class AppFrame
+    {
+    }
+}
