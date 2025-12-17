fad967d7ec87bb02b5b6dcfec5c4262df369b185
ZakharKaban <zakharkaban@gmail.com>
2025-12-17 23:39:57 +0300

    АААААА

---
+++ Project/TestingApp/TestingApp/PasswordChecker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestingApp
+{
+    public class PasswordChecker
+    {
+        public static bool ValidatePassword(string password)
+        {
+            return true;
+        }
+    }
+}
---
+++ Project/TestingApp/Testomat/PasswordCheckerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2025One.Tests
+{
+    [TestClass()]
+    public class PasswordCheckerTests
+    {
+        [TestMethod()]
+        public void ValidatePasswordTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
